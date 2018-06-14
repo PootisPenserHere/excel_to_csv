6e5e6befe5c7e4b43fb8b691d24c6bacd2f226ff
Jose Pabl Domingo Aramburo Sanchez <josepablo.aramburo@gmail.com>
2018-06-14 16:18:21 -0600

    [add] Validating file extension
    Documented the functions

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 import xlrd
 import csv
 from numbers import Number
+import os.path
 
+''' Configurations, this values will change the behaivor of the script
+and the way it interacts with the user
+'''
+global_allowedExtensions = ['.xls', '.xlsx']
+global_extensionNotAllowed = 'The extension %s is not allowed.'
+
+''' Obtains the extension of a file which already exists in disk
+
+@param filename string - the name of the file
+@return string
+'''
+def getFileExtension(fileName):
+    return os.path.splitext(fileName)[1]
+
+''' Convers an excel file into a csv
+
+@param inputFile string - includes the extension and it may be .xls or .xlsx
+@param sheet string - the name of the sheet, alternatively a number that
+represents the order of the pages can be passed where 0 would be the first sheet
+@param outputFile string - the desired name of the output file
+'''
 def excelToCsv(inputFile, sheet, outputFile):
+    fileExtension = getFileExtension(inputFile)
+
+    if fileExtension not in global_allowedExtensions:
+        raise Exception(global_extensionNotAllowed % fileExtension)
+
     wb = xlrd.open_workbook(inputFile)
 
     # Checks if the given sheet is a numer or string
