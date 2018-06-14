6a7a13f3fabb6735a9fcdff335a7711b84ed17f0
Jose Pabl Domingo Aramburo Sanchez <josepablo.aramburo@gmail.com>
2018-06-14 15:39:14 -0600

    [add] Taking sheets by number or name

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import xlrd
 import csv
+from numbers import Number
 
-def excelToCsv(inputFile, page, outputFile):
+def excelToCsv(inputFile, sheet, outputFile):
     wb = xlrd.open_workbook(inputFile)
-    sh = wb.sheet_by_index(page) #filewb.sheet_by_name('Hoja1')
-    your_csv_file = open(outputFile, 'wb')
-    wr = csv.writer(your_csv_file, quoting=csv.QUOTE_ALL)
+
+    # Checks if the given sheet is a numer or string
+    if isinstance(sheet, Number):
+        sh = wb.sheet_by_index(sheet) #Takes the sheet by its numerical order
+    else:
+        sh = wb.sheet_by_name(sheet) # Takes the sheet by its name
+
+    cvsFile = open(outputFile, 'wb')
+    wr = csv.writer(cvsFile, quoting=csv.QUOTE_ALL)
 
     for rownum in xrange(sh.nrows):
         wr.writerow(sh.row_values(rownum))
 
-    your_csv_file.close()
+    cvsFile.close()
 
-excelToCsv('sample.xlsx', 0, 'your_csv_file.csv')
+excelToCsv('sample.xlsx', 'Hoja1', 'your_csv_file.csv')
